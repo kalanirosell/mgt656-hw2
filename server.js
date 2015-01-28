be919c57d859b0b5292a0da109c0d8e05a960cfd
Kalani Rosell <kalani.rosell@yale.edu>
2015-01-28 12:01:43 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,15 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/netid', function(request, response){
+    response.send('You should put your netid here!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('smoggy herring');
+});
+
+app.listen(
+    process.env.PORT || 4000,
+    process.env.IP || 'localhost'
+);
